9106c6e1ce6b7b1700c6ae6b4c61019c5f044ca2
Andrey Kislov <neznajkaandrej99@gmail.com>
2022-09-04 15:57:22 +0300

    wrote SecurityRepositories.java and SecurityService.java

--- src/main/java/project/librarywithboot/repositories/SecurityRepositories.java
+++ src/main/java/project/librarywithboot/repositories/SecurityRepositories.java
@@ -1,4 +1,11 @@
 package project.librarywithboot.repositories;
 
-public interface SecurityRepositories {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
+import project.librarywithboot.models.SecurityPerson;
+
+import java.util.Optional;
+
+public interface SecurityRepositories extends JpaRepository<SecurityPerson, Integer> {
+  Optional<SecurityPerson> findByUsername(String username);
 }
--- src/main/java/project/librarywithboot/services/securityServices/SecurityService.java
+++ src/main/java/project/librarywithboot/services/securityServices/SecurityService.java
@@ -1,4 +1,33 @@
 package project.librarywithboot.services.securityServices;
 
-public class SecurityService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.librarywithboot.models.SecurityPerson;
+import project.librarywithboot.repositories.SecurityRepositories;
+import security.SecurityPersonDetails;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class SecurityService implements UserDetailsService {
+
+    private final SecurityRepositories securityRepositories;
+
+    public SecurityService(SecurityRepositories securityRepositories) {
+        this.securityRepositories = securityRepositories;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<SecurityPerson> optPerson = securityRepositories.findByUsername(username);
+
+        if(optPerson.isEmpty()){
+            throw new  UsernameNotFoundException("Пользователь не найден");
+        }
+        return new SecurityPersonDetails(optPerson.get());
+    }
 }
